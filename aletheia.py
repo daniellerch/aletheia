b5b904a6c2d36d9c820b9baf76ea518c33287aae
dlerch <dlerch@gmail.com>
2017-09-02 22:03:34 +0200

    add RS attack

--- aletheia.py
+++ aletheia.py
@@ -24,7 +24,7 @@ def main():
 
     if len(sys.argv)!=3:
         print sys.argv[0], "<command> <image/dir>\n"
-        print "  eof:     Find files at the end."
+        print "  eof:     Find files appended at the end."
         print "  spa:     Sample Pairs Analysis attack to LSB replacement."
         print "\n"
         sys.exit(0)
@@ -50,11 +50,36 @@ def main():
         sys.exit(0)
     # }}}
 
+    # {{{ rs
+    if sys.argv[1]=="rs":
+
+        threshold=0.05
+        bitrate_R=attacks.rs(sys.argv[2], 0)
+        bitrate_G=attacks.rs(sys.argv[2], 1)
+        bitrate_B=attacks.rs(sys.argv[2], 2)
+
+        if bitrate_R<threshold and bitrate_G<threshold and bitrate_B<threshold:
+            print "No hiden data found"
+            sys.exit(0)
+
+        if bitrate_R>=threshold:
+            print "Hiden data found in channel R", bitrate_R
+        if bitrate_G>=threshold:
+            print "Hiden data found in channel G", bitrate_G
+        if bitrate_B>=threshold:
+            print "Hiden data found in channel B", bitrate_B
+        sys.exit(0)
+    # }}}
+
+
+    if sys.argv[1]=="eof":
+        sz=attacks.extra_size(sys.argv[2])
+        sys.exit(0)
+
     if sys.argv[1]=="train-models":
         train_models()
 # }}}
 
-# 35, 47, 56, 
 
 if __name__ == "__main__":
     main()
--- aletheia/attacks.py
+++ aletheia/attacks.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
 import sys
+import numpy
 from scipy import ndimage, misc
 from cmath import sqrt
+import ntpath
+import tempfile
+
+def extra_size(filename):
+    name=ntpath.basename(filename)
+    I = misc.imread(filename)
+
+    misc.imsave(tempfile.gettempdir()+'/'+name, I)
+    return 0
+
+
+# -- SAMPLE PAIR ATTACK --
 
 # {{{ spa()
 """
@@ -42,5 +55,72 @@ def spa(filename, channel=0):
     beta=min(bp.real, bm.real)
     return beta
 # }}}
+
+
+# -- RS ATTACK --
+
+# {{{ solve()
+def solve(a, b, c):
+    sq = numpy.sqrt(b**2 - 4*a*c)
+    return ( -b + sq ) / ( 2*a ), ( -b - sq ) / ( 2*a )
+# }}}
+
+# {{{ smoothness()
+def smoothness(I):
+    return ( numpy.sum(numpy.abs( I[:-1,:] - I[1:,:] )) + 
+             numpy.sum(numpy.abs( I[:,:-1] - I[:,1:] )) )
+# }}}
+
+# {{{ groups()
+def groups(I, mask):
+    grp=[]
+    m, n = I.shape 
+    x, y = numpy.abs(mask).shape
+    for i in xrange(m-x):
+        for j in xrange(n-y):
+            grp.append(I[i:(i+x), j:(j+y)])
+    return grp
+# }}}
+
+# {{{ difference()
+def difference(I, mask):
+    cmask = - mask
+    cmask[(mask > 0)] = 0
+    L = []
+    for g in groups(I, mask):
+        flip = (g + cmask) ^ numpy.abs(mask) - cmask
+        L.append(numpy.sign(smoothness(flip) - smoothness(g)))
+    N = len(L)
+    R = float(L.count(1))/N
+    S = float(L.count(-1))/N
+    return R-S
+# }}}
+
+# {{{ rs()
+def rs(filename, channel=0):
+    I = misc.imread(filename)
+    I = I[:,:,channel]
+    I = I.astype(int)
+
+    mask = numpy.array( [[1,0],[0,1]] )
+    d0 = difference(I, mask)
+    d1 = difference(I^1, mask)
+
+    mask = -mask
+    n_d0 = difference(I, mask)
+    n_d1 = difference(I^1, mask)
+
+    p0, p1 = solve(2*(d1+d0), (n_d0-n_d1-d1-3*d0), (d0-n_d0)) 
+    if numpy.abs(p0) < numpy.abs(p1): 
+        z = p0
+    else: 
+        z = p1
+
+    return z / (z-0.5)
+# }}}
+
+
+
+
 
 
