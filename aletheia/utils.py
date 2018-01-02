63913ea5cf4ec85fc14155bc539754110dbdb4cd
Daniel Lerch Hostalot <dlerch@gmail.com>
2018-01-02 22:49:10 +0100

    function to find an executable

--- aletheia/utils.py
+++ aletheia/utils.py
@@ -34,3 +34,20 @@ def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
             for text in re.split(_nsre, s)]    
 
 
+def which(program):
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
