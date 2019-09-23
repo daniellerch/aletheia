cc260c5b8156ddf8ac73dfe933af92c34d2259a5
Daniel Lerch <dlerch@gmail.com>
2019-09-23 15:50:35 +0200

    setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup
 
 setup(
     name='Aletheia',
-    packages=['aletheia']
+    license='MIT',
+    author='Daniel Lerch Hostalot',
+    author_email='dlerch@gmail.com',
+    download_url='https://github.com/daniellerch/aletheia',
+    packages=['aletheia'],
     version='0.1',
     scripts=['aletheia.py']
 )
