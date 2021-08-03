5071c547b0ac5536a5d5ca3ad310b946cf45fd04
Daniel Lerch <dlerch@gmail.com>
2021-08-03 20:58:37 +0200

    tools: print-pixels, print-coeffs

--- aletheialib/options/tools.py
+++ aletheialib/options/tools.py
@@ -6,6 +6,8 @@ doc = "\n" \
 "  - hpf:                   High-pass filter.\n" \
 "  - print-diffs:           Differences between two images.\n" \
 "  - print-dct-diffs:       Differences between the DCT coefficients of two JPEG images.\n" \
+"  - print-pixels:          Print a range of píxels.\n" \
+"  - print-coeffs:          Print a range of JPEG coefficients.\n" \
 "  - rm-alpha:              Opacity of the alpha channel to 255.\n" \
 "  - plot-histogram:        Plot histogram.\n" \
 "  - plot-histogram-diff:   Plot histogram of differences.\n" \
@@ -147,4 +149,75 @@ def plot_dct_histogram():
     sys.exit(0)
 # }}}
 
+# {{{ print_pixels()
+def print_pixels():
+
+    if len(sys.argv)!=5:
+        print(sys.argv[0], "print-pixels <image> <width start>:<width end> <height start>:<height end>\n")
+        print("Example:")
+        print(sys.argv[0], "print-pixels test.png 400:410 200:220\n")
+        print("")
+        sys.exit(0)
+
+    import imageio
+    I = imageio.imread(sys.argv[2])
+
+    w = sys.argv[3].split(":")
+    h = sys.argv[4].split(":")
+    ws = int(w[0])
+    we = int(w[1])
+    hs = int(h[0])
+    he = int(h[1])
+
+
+    if len(I.shape) == 2:
+        print("Image shape:", I.shape[:2])
+        print(I[hs:he, ws:we])
+    else:
+        print("Image shape:", I.shape[:2])
+        for ch in range(I.shape[2]):
+            print("Channel:", ch)
+            print(I[hs:he, ws:we, ch])
+            print()
+
+# }}}
+
+# {{{ print_coeffs()
+def print_coeffs():
+
+    if len(sys.argv)!=5:
+        print(sys.argv[0], "print-coeffs <image> <width start>:<width end> <height start>:<height end>\n")
+        print("Example:")
+        print(sys.argv[0], "print-coeffs test.jpg 400:410 200:220\n")
+        print("")
+        sys.exit(0)
+
+    w = sys.argv[3].split(":")
+    h = sys.argv[4].split(":")
+    ws = int(w[0])
+    we = int(w[1])
+    hs = int(h[0])
+    he = int(h[1])
+
+
+    fn, ext = os.path.splitext(sys.argv[2])
+    if ext[1:].lower() not in ["jpg", "jpeg"]:
+        print("ERROR: Please, provide a JPEG image")
+        sys.exit(0)
+
+    import aletheialib.utils
+    from aletheialib.jpeg import JPEG
+
+    img = aletheialib.utils.absolute_path(sys.argv[2])
+    im_jpeg = JPEG(img)
+
+    for i in range(im_jpeg.components()):
+        coeffs = im_jpeg.coeffs(i)
+        print("Image shape:", coeffs.shape)
+        print("Channel:", i)
+        print(coeffs[hs:he, ws:we])
+        print()
+
+
+# }}}
 
