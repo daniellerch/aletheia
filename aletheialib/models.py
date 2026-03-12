a82a05fb992d241859c6c6447b14ba1a8067e8f0
Daniel Lerch <dlerch@gmail.com>
2026-03-12 10:44:09 +0100

    fix effnet initializacion missing path

--- aletheialib/models.py
+++ aletheialib/models.py
@@ -285,13 +285,19 @@ class NN:
         from tensorflow.keras import mixed_precision
         mixed_precision.set_global_policy('mixed_float16')
 
+        base_model = efn.EfficientNetB0(
+            input_shape=input_shape,
+            weights=None,
+            include_top=False
+        )
+
+        init_path = os.path.join(self.model_dir, "effnetb0-init.h5")
+        if not os.path.isfile(init_path):
+            raise FileNotFoundError(f"Missing local EfficientNet weights: {init_path}")
+        base_model.load_weights(init_path)
 
         model = tf.keras.Sequential([
-            efn.EfficientNetB0(
-                input_shape=input_shape,
-                weights='imagenet',
-                include_top=False
-                ),
+            base_model,
             L.GlobalAveragePooling2D(),
 
             L.Dense(2, activation='softmax', dtype="float32")
@@ -882,4 +888,3 @@ def dci_si_method(image_path, method, modelA=None, modelB=None):
 
 
 # }}}
-
